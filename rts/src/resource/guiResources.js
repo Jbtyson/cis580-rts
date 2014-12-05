9025be412693192167cd0c360433110d7cf96338
Jbtyson <jbtyson@ksu.edu>
2014-12-05 12:20:38 -0600

    added base resource framework

--- rts/src/resource/guiResources.js
+++ rts/src/resource/guiResources.js
@@ -5,10 +5,29 @@ GuiResources = function() {
 	this.loading = 0;
 	
 	this.img = {
-		//image: new Image(),
+		// Minimap
+		minimapBackground: new Image(),
+		
+		// Unit Panel
+		unitPanelBackground: new Image(),
+		unitPortraitBackground: new Image(),
+		
+		// Captain Portrait
+		captainBackground: new Image(),
+		captainForeground: new Image(),
+		
+		// Command Panel
+		buttonBackground: new Image(),
+		commandBackground: new Image(),
+		
+		// Resource Bar
+		resourceBarBackground: new Image(),
+		minerals: new Image(),
+		gas: new Image(),
+		supply: new Image(),
 	};
 	this.sfx = {
-		//audio: new Audio(),
+		// Need some button click sounds
 	};
 }
 
