55086293e0500ccffed3e3be6d4a1ff702e59981
Alex L'Esperance <alesperance22@gmail.com>
2014-12-13 17:24:09 -0600

    Added the Scootching of the Screen

--- rts/src/ui/minimap.js
+++ rts/src/ui/minimap.js
@@ -1,11 +1,12 @@
 // Minimap.js
-// James Tyson
+// James Tyson & Alex Lesperance
 var Minimap = function() {
 	this.image = Resource.gui.img.minimap;
 
 	this.dimensions = { width:128, height:128 }
 	this.position = { x:0, y: HEIGHT - this.dimensions.height }
 	this.hitbox = new Rectangle(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
+	
 }
 
 Minimap.prototype = {
@@ -17,7 +18,9 @@ Minimap.prototype = {
 		context.save();
 		context.drawImage(this.image, this.position.x, this.position.y);
 		context.strokeStyle = 'green';
-		context.strokeRect(this.position.x, this.position.y, 64, 64);
+		context.strokeRect(this.position.x + (globalx / 10), this.position.y + (globaly / 10), 64, 64);
 		context.restore();
+		console.log(globalx);
+		console.log(globaly);
 	},
 }
