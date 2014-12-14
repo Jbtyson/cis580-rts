6461f596af18cdb02aa118c923cf39d8412e5a51
Ninjtastic <ryanwoodburn92@gmail.com>
2014-12-14 17:18:30 -0600

    Made it so buildings create hoplites

--- rts/src/units/hoplite.js
+++ rts/src/units/hoplite.js
@@ -1,6 +1,6 @@
 // max erdwien
 //Ryan Woodburn: Added a range of 0, added GetAttackRange() method
-var Hoplite = function(x, y, color, game) {
+var Hoplite = function(x, y, faction, game) {
 	this.game = game;
 
 	this.maxhealth = 60;
@@ -16,7 +16,7 @@ var Hoplite = function(x, y, color, game) {
 	
 	this.x = x;
 	this.y = y;
-	this.color = color;
+	this.faction = faction;
 	
 	//this.render = HopliteRender;
 	//this.update = HopliteUpdate;
@@ -26,34 +26,6 @@ var Hoplite = function(x, y, color, game) {
 }
 
 Hoplite.prototype = new Unit(100,100,60,"#000000");
-
-Hoplite.prototype.render = function(ctx) {
-	var self = this;
-
-	ctx.save();
-	ctx.beginPath();
-	if (self.selected) {
-		ctx.strokeStyle = "#00FF00";
-	} else {
-		ctx.strokeStyle = "#000000";
-	}
-	ctx.lineWidth = self.borderwidth;
-	ctx.fillStyle = self.color;
-	ctx.beginPath();
-	ctx.arc(self.x-globalx, self.y-globaly, self.radius-(self.borderwidth/2), 0, 2*Math.PI, false);
-	ctx.fill();
-	ctx.stroke();
-	
-	// draw health bar
-	var maxbarlength = 16;
-	var barheight = 4;
-	var barlength = maxbarlength * (self.health/self.maxhealth);
-	ctx.fillStyle = "#00FF00";
-	ctx.beginPath();
-	ctx.rect(self.x-(maxbarlength/2)-globalx, self.y-(barheight/2)-globaly,	barlength, barheight);
-	ctx.fill();
-	ctx.restore();
-}
 
 Hoplite.prototype.update = function(elapsedTime) {
 	var self = this;
@@ -116,13 +88,13 @@ Hoplite.prototype.update = function(elapsedTime) {
 	else if (self.mode == "idle") {
 		self.game.factions.forEach( function(faction) {
 			for (var i = 0; i <  faction.units.length; i++) {
-				if (faction.units[i].color != self.color &&
+				if (faction.units[i].faction != self.faction &&
 						self.game.cd.detect(self, faction.units[i])) {
 					self.attack(faction.units[i]);
 				}
-				else if (game.units[i].faction == self.faction &&
-						game.cd.detect(self, game.units[i]) && self != game.units[i] &&game.units[i].mode == "idle") {
-					self.loseStack(game.units[i]);
+				else if (faction.units[i].faction == self.faction &&
+						game.cd.detect(self, faction.units[i]) && self != faction.units[i] && faction.units[i].mode == "idle") {
+					self.loseStack(faction.units[i]);
 				}
 			}
 		});
