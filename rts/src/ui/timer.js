668cb8ac50a204a305dccd2330ac3a858eb7e787
Alex L'Esperance <alesperance22@gmail.com>
2014-12-15 17:33:14 -0600

    Padded the time if it is a single digit.

--- rts/src/ui/timer.js
+++ rts/src/ui/timer.js
@@ -6,9 +6,9 @@ var Timer = function()
 	this.position = {x: 0, y: HEIGHT - 128 - this.dimensions.height }
 	this.hitbox = new Rectangle(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
 	this.runningTime = 0;
-	this.seconds = 0.0;
-	this.minutes = 0.0;
-	this.hours = 0;
+	this.seconds = 0;
+	this.minutes = 9;
+	this.hours = 0.;
 }
 
 Timer.prototype = 
@@ -41,7 +41,17 @@ Timer.prototype =
 		
 		context.fillStyle = "yellow";
 		context.font = "bold 14px Arial";
-		context.fillText(this.hours + ":" + this.minutes + ":" + this.seconds, this.position.x + 16 , this.position.y + 12);
+		context.fillText(this.hours + ":" + pad(this.minutes, 2) + ":" + pad(this.seconds, 2), this.position.x + 8 , this.position.y + 12);
 		context.restore();
 	}
+}
+
+
+//Pad the number with a zero if it is a single digit number
+//stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
+function pad(n, width, z)
+{
+	z = z || '0';
+	n = n + '';
+	return n.length >= width? n : new Array(width - n.length + 1).join(z) + n;
 }
