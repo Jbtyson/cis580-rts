441d7ed951c5a43b88413c597b2af98277db62a2
unknown <alexl22@N126W72.win.cis.ksu.edu>
2014-12-15 17:18:37 -0600

    Have a working timer.

--- rts/src/ui/timer.js
+++ rts/src/ui/timer.js
@@ -5,14 +5,32 @@ var Timer = function()
 	this.dimensions = { width: 64, height: 16 }
 	this.position = {x: 0, y: HEIGHT - 128 - this.dimensions.height }
 	this.hitbox = new Rectangle(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
+	this.runningTime = 0;
+	this.seconds = 0.0;
+	this.minutes = 0.0;
+	this.hours = 0;
 }
 
 Timer.prototype = 
 {
 	update: function(gameTime)
 	{
-		//http://stackoverflow.com/questions/2604450/how-to-create-a-jquery-clock-timer
-		//http://jsfiddle.net/smallworld/ng22Y/1/
+		this.runningTime += gameTime;
+		if(this.runningTime >= 1000)
+		{
+			this.seconds++;
+			if(this.seconds == 60)
+			{
+				this.minutes++;
+				this.seconds = 0;
+				if(this.minutes == 60)
+				{
+					this.hours++;
+					this.minutes = 0;
+				}
+			}
+			this.runningTime = 0;
+		}
 	},
 	
 	render: function(context)
@@ -20,6 +38,10 @@ Timer.prototype =
 		context.save();
 		context.fillStyle = "black"
 		context.fillRect(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
+		
+		context.fillStyle = "yellow";
+		context.font = "bold 14px Arial";
+		context.fillText(this.hours + ":" + this.minutes + ":" + this.seconds, this.position.x + 16 , this.position.y + 12);
 		context.restore();
 	}
 }
