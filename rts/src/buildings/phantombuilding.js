a32c6e909d957f4b9642e8eec0e4db15986a366a
delpire <chrisdelpire@gmail.com>
2014-12-17 04:15:37 -0600

    Buildings can now be built in a proper location

--- rts/src/buildings/phantombuilding.js
+++ rts/src/buildings/phantombuilding.js
@@ -45,7 +45,9 @@ PhantomBuilding.prototype = {
 
 	update: function(elapsedTime){
 	   
-		for(var i = 0; i < this.game.factions[0].buildings.length; i++){
+	  var currentAmountOfBuildings = this.game.factions[0].buildings.length;
+	  
+		for(var i = 0; i < currentAmountOfBuildings ; i++){
 
 			// Calculate how far away the phantom is from a building.
 			var distance_x = (this.game.input.mousex - globalx) - (this.game.factions[0].buildings[i].world_x + BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].width / 2);
@@ -60,49 +62,85 @@ PhantomBuilding.prototype = {
 
 					if(distance_x < 0){
 						this.orientation = 2;
+						
+						if(this.game.tryToBuild && (this.game.factions[0].buildings[i].orientation == this.orientation || this.game.factions[0].buildings[i].type === 0)){
+  						  buildNewBuilding( this.game.factions[0].buildings[i].world_x - BUILDING_SPRITE_DATA[this.type].width,
+  						                    this.game.factions[0].buildings[i].world_y + ((BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].height - BUILDING_SPRITE_DATA[this.type].height) / 2),
+  						                    this.type, this.game.factions[0].buildings, this.orientation, this.game);
+  					
+  					  
+  				    this.game.phantom = null;
+  					}
 					}
 					else{
 						this.orientation = 0;
+						
+						// If the building is close enough to be built, the correct orientation, and the player clicks.
+						// Then try to build the building.
+  					if(this.game.tryToBuild && (this.game.factions[0].buildings[i].orientation == this.orientation || this.game.factions[0].buildings[i].type === 0)){
+  						  buildNewBuilding( this.game.factions[0].buildings[i].world_x + BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].width,
+  						                    this.game.factions[0].buildings[i].world_y + ((BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].height - BUILDING_SPRITE_DATA[this.type].height) / 2),
+  						                    this.type, this.game.factions[0].buildings, this.orientation, this.game);
+  					
+  					  
+  				    this.game.phantom = null;
+  					}
 					}
-
-					// If the building is close enough, and the user tries to build
-					// then build the building in the correct location.
-					if(this.game.tryToBuild){
-						this.game.factions[0].buildings.push( new Connector(this.game.factions[0].buildings[i].world_x + (BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].width * Math.sign(distance_x)),
-																			this.game.factions[0].buildings[i].world_y + ((BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].height - BUILDING_SPRITE_DATA[this.type].height) / 2),
-																			this.orientation, 0, this.game));
-
-						this.game.phantom = null;
-					}
-
-
 				}
 
 				if(Math.abs(distance_x) < Math.abs(distance_y)){
 
 					if(distance_y < 0){
 						this.orientation = 3;
+
+						// If the building is close enough to be built, the correct orientation, and the player clicks.
+						// Then try to build the building.
+						if(this.game.tryToBuild && (this.game.factions[0].buildings[i].orientation == this.orientation || this.game.factions[0].buildings[i].type === 0)){
+  						  buildNewBuilding( this.game.factions[0].buildings[i].world_x + ((BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].width - BUILDING_SPRITE_DATA[this.type].width) / 2),
+  						                    this.game.factions[0].buildings[i].world_y - BUILDING_SPRITE_DATA[this.type].height,
+  						                    this.type, this.game.factions[0].buildings, this.orientation, this.game);
+  					
+  					  
+  				    this.game.phantom = null;
+  					}
 					}
 					else{
 						this.orientation = 1;
-					}
 
-					// If the building is close enough, and the user tries to build
-					// then build the building in the correct location.
-					if(this.game.tryToBuild){
-						this.game.factions[0].buildings.push( new Connector(this.game.factions[0].buildings[i].world_x + ((BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].height - BUILDING_SPRITE_DATA[this.type].height) / 2),
-																			this.game.factions[0].buildings[i].world_y + (BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].width * Math.sign(distance_y)),
-																			this.orientation, 0, this.game));
-
-						this.game.phantom = null;
+						// If the building is close enough to be built, the correct orientation, and the player clicks.
+						// Then try to build the building.
+						if(this.game.tryToBuild && (this.game.factions[0].buildings[i].orientation == this.orientation || this.game.factions[0].buildings[i].type === 0)){
+  						  buildNewBuilding( this.game.factions[0].buildings[i].world_x + ((BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].width - BUILDING_SPRITE_DATA[this.type].width) / 2),
+  						                    this.game.factions[0].buildings[i].world_y + BUILDING_SPRITE_DATA[this.game.factions[0].buildings[i].type].height,
+  						                    this.type, this.game.factions[0].buildings, this.orientation, this.game);
+  					
+  					  
+  				    this.game.phantom = null;
+  					}
 					}
 				}
 			}
-
-			this.game.tryToBuild = false;
-
-
 		}
+			this.game.tryToBuild = false;
 	},
 
+}
+
+//Helper function to build new buildings.
+var buildNewBuilding = function(x, y, type, buildingArray, orientation, game){
+
+  switch(type){
+    
+    case 0:
+      buildingArray.push( new Towncenter(x, y, orientation, 0, game));
+      break;
+    case 1:
+      buildingArray.push( new Connector(x, y, orientation, 0, game));
+      break;
+    case 2:
+      buildingArray.push( new Barracks(x, y, orientation, 0, game));
+      break;
+    default:
+    
+  }
 }
